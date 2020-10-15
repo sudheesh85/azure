7aa5e2f330f8b2639fe1c006f5691e1da3941e07
sudheesh85 <sudheesh85@gmail.com>
2020-10-14 21:49:25 -0500

    modified

--- django/deal/deal/settings.py
+++ django/deal/deal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ara1)&%#p00fixs&52mpdj6qr$&r2e8#r*lm9xw_5u5c7$4*)+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
